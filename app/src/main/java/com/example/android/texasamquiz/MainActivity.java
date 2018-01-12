6cfdb3f61b42d6cc376552ea033966c645793cec
jayton13 <jayton13@gmail.com>
2018-01-12 08:28:59 -0600

    updated to remove find view by id from sections

--- app/src/main/java/com/example/android/texasamquiz/MainActivity.java
+++ app/src/main/java/com/example/android/texasamquiz/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.function.ToIntFunction;
@@ -14,6 +15,24 @@ public class MainActivity extends AppCompatActivity {
     float numQuestionsTotal = 5;
     float numQestionsCorrect = 0;
     int answer;
+    RadioGroup radiogroup1;
+    RadioGroup radiogroup2;
+    RadioGroup radiogroup3;
+    RadioGroup radiogroup4;
+    RadioGroup radiogroup5;
+    RadioButton radiobutton1a;
+    RadioButton radiobutton1b;
+    RadioButton radiobutton1c;
+    RadioButton radiobutton2a;
+    RadioButton radiobutton2b;
+    RadioButton radiobutton2c;
+    RadioButton radiobutton3a;
+    RadioButton radiobutton3b;
+    RadioButton radioButton4a;
+    RadioButton radiobutton4b;
+    RadioButton radiobutton4c;
+    RadioButton radiobutton5a;
+    RadioButton radiobutton5b;
     int correctId;
     float score;
     String toastMessage;
@@ -22,47 +41,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        radiogroup1 = (RadioGroup)findViewById(R.id.radiogroup1);
+        radiogroup2 = (RadioGroup)findViewById(R.id.radiogroup2);
+        radiogroup3 = (RadioGroup)findViewById(R.id.radiogroup3);
+        radiogroup4 = (RadioGroup)findViewById(R.id.radiogroup4);
+        radiogroup5 = (RadioGroup)findViewById(R.id.radiogroup5);
+
+        radiobutton1a = (RadioButton)findViewById(R.id.radiobutton1a);
+        radiobutton1b = (RadioButton)findViewById(R.id.radiobutton1b);
+        radiobutton1c = (RadioButton)findViewById(R.id.radiobutton1c);
+        radiobutton2a = (RadioButton)findViewById(R.id.radiobutton2a);
+        radiobutton2b = (RadioButton)findViewById(R.id.radiobutton2b);
+        radiobutton2c = (RadioButton)findViewById(R.id.radiobutton2c);
+        radiobutton3a = (RadioButton)findViewById(R.id.radiobutton3a);
+        radiobutton3b = (RadioButton)findViewById(R.id.radiobutton3b);
+        radioButton4a = (RadioButton)findViewById(R.id.radiobutton4a);
+        radiobutton4b = (RadioButton)findViewById(R.id.radiobutton4b);
+        radiobutton4c = (RadioButton)findViewById(R.id.radiobutton4c);
+        radiobutton5a = (RadioButton)findViewById(R.id.radiobutton5a);
+        radiobutton5b = (RadioButton)findViewById(R.id.radiobutton5b);
+
     }
 
     public void checkAnswers(View view)
     {
-        RadioGroup radioGroup1 = (RadioGroup)(findViewById(R.id.question1));
-        answer = radioGroup1.getCheckedRadioButtonId();
-        RadioButton radioButton1 = (RadioButton) findViewById(R.id.answer1B);
-        correctId = radioButton1.getId();
-        if(answer == correctId){
+        correctId = radiobutton1b.getId();
+        answer = radiogroup1.getCheckedRadioButtonId();
+        if(radiogroup1.getCheckedRadioButtonId()==radiobutton1b.getId())
+        {
             numQestionsCorrect = numQestionsCorrect + 1;
         }
 
-        RadioGroup radioGroup2 = (RadioGroup)(findViewById(R.id.question2));
-        answer = radioGroup2.getCheckedRadioButtonId();
-        RadioButton radioButton2 = (RadioButton) findViewById(R.id.answer2A);
-        correctId = radioButton2.getId();
-        if(answer == correctId){
+        if(radiogroup2.getCheckedRadioButtonId()==radiobutton2a.getId()){
             numQestionsCorrect = numQestionsCorrect + 1;
         }
 
-        RadioGroup radioGroup3 = (RadioGroup)(findViewById(R.id.question3));
-        answer = radioGroup3.getCheckedRadioButtonId();
-        RadioButton radioButton3 = (RadioButton) findViewById(R.id.answer3A);
-        correctId = radioButton3.getId();
-        if(answer == correctId){
+        if(radiogroup3.getCheckedRadioButtonId()==radiobutton3a.getId()){
             numQestionsCorrect = numQestionsCorrect + 1;
         }
 
-        RadioGroup radioGroup4 = (RadioGroup)(findViewById(R.id.question4));
-        answer = radioGroup4.getCheckedRadioButtonId();
-        RadioButton radioButton4 = (RadioButton) findViewById(R.id.answer4B);
-        correctId = radioButton4.getId();
-        if(answer == correctId){
-            numQestionsCorrect = numQestionsCorrect + 1;
+        if(radiogroup4.getCheckedRadioButtonId()==radiobutton4b.getId()){
+        numQestionsCorrect = numQestionsCorrect + 1;
         }
 
-        RadioGroup radioGroup5 = (RadioGroup)(findViewById(R.id.question5));
-        answer = radioGroup5.getCheckedRadioButtonId();
-        RadioButton radioButton5 = (RadioButton) findViewById(R.id.answer5A);
-        correctId = radioButton5.getId();
-        if(answer == correctId){
+        if(radiogroup5.getCheckedRadioButtonId()==radiobutton5a.getId()){
             numQestionsCorrect = numQestionsCorrect + 1;
         }
 
@@ -75,11 +98,11 @@ public class MainActivity extends AppCompatActivity {
 
         //Reset form
         numQestionsCorrect = 0;
-        radioGroup1.clearCheck();
-        radioGroup2.clearCheck();
-        radioGroup3.clearCheck();
-        radioGroup4.clearCheck();
-        radioGroup5.clearCheck();
+        radiogroup1.clearCheck();
+        radiogroup2.clearCheck();
+        radiogroup3.clearCheck();
+        radiogroup4.clearCheck();
+        radiogroup5.clearCheck();
 
 
     }
